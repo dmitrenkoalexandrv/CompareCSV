2436d0c8e416e3b353da69b8bb3e77191bd647b4
Oleksadr Dmytrenko <dmitrenkoalexandrv@gmail.com>
2018-04-29 13:44:38 +0300

    Commit 3

---
+++ CompareCSV/Compare.BL.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComapreCSV
+{
+    static class Compare
+    {
+        static public List<string[]> parseCSV(string path)
+        {
+            List<string[]> parsedData = new List<string[]>();
+
+            try
+            {
+                using (StreamReader readFile = new StreamReader(path))
+                {
+                    string line;
+                    string[] row;
+
+                    while ((line = readFile.ReadLine()) != null)
+                    {
+                        row = line.Split(';');
+                        parsedData.Add(row);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+            return parsedData;
+        }
+
+
+        public static DataTable ConvertListToDataTable(List<string[]> list)
+        {
+            // New table.
+            DataTable table = new DataTable();
+
+            // Get max columns.
+            int columns = 0;
+            foreach (var array in list)
+            {
+                if (array.Length > columns)
+                {
+                    columns = array.Length;
+                }
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                string columnName = "";
+                if (i == 0)
+                {
+                    columnName = "ColumnName";
+                }
+                else if (i == 1)
+                {
+                    columnName = "Value in Column with Max Length";
+                }
+                else if (i == 2)
+                {
+                    columnName = "Suggested Type";
+                }
+                else if (i == 3)
+                {
+                    columnName = "Max Lenght";
+                }
+
+                table.Columns.Add(columnName);
+            }
+
+            // Add transpond rows.
+            for (int i = 0; i < columns; i++)
+            {
+                table.Rows.Add(list[0].ElementAt(i), GetFirstValueFromListOfArrraysByMaxLentgh(list, ColMaxLenght(list, i),i), TryParseStringInListOfArrrays(list[1].ElementAt(i), i), ColMaxLenght(list, i));
+            }
+
+            return table;
+        }
+
+
+        public static int ColMaxLenght(List<string[]> arr, int i)
+        {
+            return arr.Skip(1).Max(p => p[i].Length);
+        }
+
+
+        public static string TryParseStringInListOfArrrays(string value, int i)
+        {
+            Double dbl;
+            Int32 integ;
+            Int64 integ64;
+            String caseSwitch = "";
+
+            bool resultInt = Int32.TryParse(value.ToString().Replace('.', ','), out integ);
+            bool resultInt64 = Int64.TryParse(value.ToString().Replace('.', ','), out integ64);
+            bool resultDouble = Double.TryParse(value.ToString().Replace('.', ','), out dbl);
+
+
+            if (resultInt == true)
+            {
+                caseSwitch = "System.Int32";
+            }
+            else if (resultInt64 == true)
+            {
+                caseSwitch = "System.Int64";
+            }
+            else if (resultDouble == true)
+            {
+                caseSwitch = "System.Double";
+            }
+            else
+            {
+                caseSwitch = "System.String";
+            }
+
+            return caseSwitch;
+        }
+
+
+        public static string GetFirstValueFromListOfArrraysByMaxLentgh(List<string[]> array, int maxLentgh, int i)
+        {
+            return array.Skip(1).First(p => p[i].Length == maxLentgh).ElementAt(i);
+        }
+
+    }
+}
---
+++ CompareCSV/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComapreCSV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            List<string[]> res = Compare.parseCSV(@"c:\tmp\ex.csv");
+            DataTable table = Compare.ConvertListToDataTable(res);
+            dataGridView1.DataSource = table;
+
+            List<string[]> res_1 = Compare.parseCSV(@"c:\tmp\ex_1.csv");
+            DataTable table_1 = Compare.ConvertListToDataTable(res_1);
+            dataGridView2.DataSource = table_1;
+
+            this.ActiveControl = button1;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var list_1 = dataGridView1.Rows.Cast<DataGridViewRow>().Where(row => row.Index >= 0) ;
+            var list_2 = dataGridView2.Rows.Cast<DataGridViewRow>().Where(row => row.Index >= 0);
+
+            foreach (DataGridViewRow row in dataGridView2.Rows)
+            {
+                if (!list_1.Any(s => s.Cells["ColumnName"].Value.Equals(row.Cells["ColumnName"].Value)))
+                {
+                    row.Cells["ColumnName"].Style.BackColor = Color.Cyan;
+                }
+
+                if (
+                    list_1.Where(s => s.Cells["ColumnName"].Value.Equals(row.Cells["ColumnName"].Value)).Any(n => !n.Cells["Value in Column with Max Length"].Value.Equals(row.Cells["Value in Column with Max Length"].Value))
+                    )
+                {
+                    row.Cells["Value in Column with Max Length"].Style.BackColor = Color.Lavender;
+                }
+
+                if (
+                    list_1.Where(s => s.Cells["ColumnName"].Value.Equals(row.Cells["ColumnName"].Value)).Any(n => !n.Cells["Suggested Type"].Value.Equals(row.Cells["Suggested Type"].Value))
+                    )
+                {
+                    row.Cells["Suggested Type"].Style.BackColor = Color.Lavender;
+                }
+
+                if (
+                    list_1.Where(s => s.Cells["ColumnName"].Value.Equals(row.Cells["ColumnName"].Value)).Any(n => !n.Cells["Max Lenght"].Value.Equals(row.Cells["Max Lenght"].Value))
+                    )
+                {
+                    row.Cells["Max Lenght"].Style.BackColor = Color.Lavender;
+                }
+            }
+
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (!list_2.Any(s => s.Cells["ColumnName"].Value.Equals(row.Cells["ColumnName"].Value)))
+                {
+                    row.Cells["ColumnName"].Style.BackColor = Color.Coral;
+                }
+
+                if (
+                    list_2.Where(s => s.Cells["ColumnName"].Value.Equals(row.Cells["ColumnName"].Value)).Any(n => !n.Cells["Value in Column with Max Length"].Value.Equals(row.Cells["Value in Column with Max Length"].Value))
+                    )
+                {
+                    row.Cells["Value in Column with Max Length"].Style.BackColor = Color.Lavender;
+                }
+
+                if (
+                    list_2.Where(s => s.Cells["ColumnName"].Value.Equals(row.Cells["ColumnName"].Value)).Any(n => !n.Cells["Suggested Type"].Value.Equals(row.Cells["Suggested Type"].Value))
+                    )
+                {
+                    row.Cells["Suggested Type"].Style.BackColor = Color.Lavender;
+                }
+
+                if (
+                    list_2.Where(s => s.Cells["ColumnName"].Value.Equals(row.Cells["ColumnName"].Value)).Any(n => !n.Cells["Max Lenght"].Value.Equals(row.Cells["Max Lenght"].Value))
+                    )
+                {
+                    row.Cells["Max Lenght"].Style.BackColor = Color.Lavender;
+                }
+            }
+            
+            //???
+            var result = dataGridView1.Rows.Cast<DataGridViewRow>().Select(p => p.Cells["ColumnName"].Value.ToString()).ToList().Intersect(dataGridView2.Rows.Cast<DataGridViewRow>().Select(p => p.Cells["ColumnName"].Value.ToString()).ToList());
+            //dataGridView1.Rows.Cast<DataGridViewRow>().Where(p => p.Cells["ColumnName"].Value.ToString().ToList().Intersect(result)).Selected = true;
+
+            dataGridView1.ClearSelection();
+            dataGridView2.ClearSelection();
+        }
+
+
+        
+
+    
+    }
+}
